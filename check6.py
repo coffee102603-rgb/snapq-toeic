358c0f9edd3693df63bfdb9beb08588b32acea94
Choi Jeongeun <your@email.com>
2026-04-02 13:25:04 +0900

    chore: 임시 스크립트 파일 정리

--- check6.py
+++
@@ -1,6 +0,0 @@
-﻿with open("C:/Users/최정은/Desktop/snapq_toeic_V3/pages/04_P7_Reading.py", "r", encoding="utf-8-sig") as f:
-    content = f.read()
-
-# 현재 멘트 찾기
-idx = content.find("P7예문=P5문제")
-print(repr(content[idx-80:idx+100]))
--- check7.py
+++
@@ -1,7 +0,0 @@
-﻿with open("C:/Users/최정은/Desktop/snapq_toeic_V3/pages/04_P7_Reading.py", "r", encoding="utf-8-sig") as f:
-    content = f.read()
-
-idx = content.find("무기 장착")
-while idx != -1:
-    print(repr(content[idx-10:idx+40]))
-    idx = content.find("무기 장착", idx+1)
